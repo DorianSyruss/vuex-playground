cf9e62066b4ce7deb05aefad0bac22479263f2d8
Ivan Saric <tcd-kakaroto@hotmail.com>
2018-09-08 23:41:17 +0200

    Improve webpack dev server info log, gitignore build files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -47,7 +47,6 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true,
     overlay: true
   },
   performance: {
