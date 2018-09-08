3d241e97ab77c4eb47aac50ad780f7d879517205
Ivan Saric <tcd-kakaroto@hotmail.com>
2018-09-08 22:48:20 +0200

    Add stylelint config for js and vue

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    'browser': true,
+    'commonjs': true,
+    'es6': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/base'
+  ],
+  parserOptions: {
+    'sourceType': 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
