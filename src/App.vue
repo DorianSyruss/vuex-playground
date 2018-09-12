41c592c60c5a9f96b4cbc3d8b6e24d53919ec4f6
Ivan Saric <tcd-kakaroto@hotmail.com>
2018-09-12 21:49:45 +0200

    Introduce vuex store and define products data in the central state

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,7 @@ import ProductListTwo from './components/ProductListTwo';
 export default {
   name: 'app',
   data() {
-    return {
-      products: [
-        { name: 'Banana Skin', price: 20 },
-        { name: 'Shiny Star', price: 40 },
-        { name: 'Green Shells', price: 60 },
-        { name: 'Red Shells', price: 80 },
-      ]
-    };
+    return {};
   },
   components: {
     ProductListOne,
