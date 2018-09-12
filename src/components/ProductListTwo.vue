96c8f9b34b772c85889497036e0d0327627ff379
Ivan Saric <tcd-kakaroto@hotmail.com>
2018-09-12 22:39:15 +0200

    Introducing the concept of getters
    - get all items on sale from the store by using saleProducts getter
    - easily propagate the data change through components (if the price keeps changing)

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
   <div class="product-list-two">
     <h2>Product List Two</h2>
     <ul>
-      <li v-for="product in products" :key="product.name">
+      <li v-for="product in saleProducts" :key="product.name">
         <span class="name">{{ product.name }}</span>
         <span class="price">{{ product.price }}</span>
       </li>
@@ -15,6 +15,9 @@ export default {
   computed: {
     products() {
       return this.$store.state.products;
+    },
+    saleProducts() {
+      return this.$store.getters.saleProducts;
     }
   }
 };
